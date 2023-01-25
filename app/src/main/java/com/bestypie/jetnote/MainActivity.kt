ee7a9b5507d9d417ce928cbc121a64271e4b8f3d
Karumaidoi <mainahmwangi12@gmail.com>
2023-01-25 08:52:22 +0300

    state management

--- app/src/main/java/com/bestypie/jetnote/MainActivity.kt
+++ app/src/main/java/com/bestypie/jetnote/MainActivity.kt
@@ -3,6 +3,7 @@ package com.bestypie.jetnote
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,9 +13,11 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bestypie.jetnote.data.NotesDataSource
 import com.bestypie.jetnote.model.Note
 import com.bestypie.jetnote.screen.NoteScreen
+import com.bestypie.jetnote.screen.NoteViewModel
 import com.bestypie.jetnote.ui.theme.JetNoteTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,21 +30,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val notes = remember {
-                        mutableStateListOf<Note>()
-                    }
+                    val noteViewModel: NoteViewModel by viewModels()
+                    NotesApp(noteViewModel)
 
-                    NoteScreen(notes = notes, onAddNote = {notes.add(it)
-                    }, onRemoveNote = {
-                        notes.remove(it)
-                    })
+
                 }
             }
         }
     }
 }
 
-
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
+    val notesList = noteViewModel.getAllNotes();
+    NoteScreen(notes = notesList, onAddNote = {noteViewModel.addNote(it)
+    }, onRemoveNote = {
+        noteViewModel.removeNote(it)
+    })
+}
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/bestypie/jetnote/screen/NoteViewModel.kt
+++ app/src/main/java/com/bestypie/jetnote/screen/NoteViewModel.kt
@@ -1,10 +1,15 @@
 package com.bestypie.jetnote.screen
 
 import androidx.lifecycle.ViewModel
+import com.bestypie.jetnote.data.NotesDataSource
 import com.bestypie.jetnote.model.Note
 
 class NoteViewModel: ViewModel() {
     private var noteList = mutableListOf<Note>()
+
+    init {
+        noteList.addAll(NotesDataSource().loadNotes())
+    }
 
     fun addNote(note: Note) {
         noteList.add(note);
